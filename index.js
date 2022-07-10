7b22244bfb88f0714b08fda03123b591095d1b97
Nura-Programmer <nuraprogrammer@gmail.com>
2022-07-10 04:50:56 +0000

    Add error and other date format feature

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyPaser.urlencoded({extended: false}));
+app.use(bodyPaser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 app.use((req, _, next) => {
-  const {method, path, ip} = req;
+  const { method, path, ip } = req;
   const logDetails = `${method} ${path} - ${ip}`;
 
   console.log(logDetails);
@@ -18,17 +18,34 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/api/:date", (req, res) => {
-  let  date  = req.params.date;
-  date = date.length > 10 ? Number(date) : date;
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
   
+  res.statusCode = 200;
+  res.contentType = "application/json";
+
+  if (date === undefined) {
+    const currentDate = new Date();
+
+
+    return res.json({
+      unix: Math.floor(currentDate.getTime() / 1000),
+      utc: currentDate.toGMTString()
+    });
+  }
+
+  date = date.indexOf(",") > -1 ? decodeURIComponent(date) : date.length > 10 ? Number(date) : date;
+
+  console.log(date);
+  
+  if (date === NaN) return res.status(400).json({ error: "Invalid Date" });
+
   const gmtDate = new Date(date);
-  const response = {
+
+  res.json({
     unix: Math.floor(gmtDate.getTime() / 1000),
     utc: gmtDate.toGMTString()
-  };
-    
-    res.json(response);
+  });
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
