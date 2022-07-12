94ba0098a2351e8069374bd05a7174a1bff65123
Nura Programmer <nuraprogrammer@gmail.com>
2022-07-12 04:37:26 +0000

    Refaactoring

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const cors = require("cors");
 const express = require("express");
-const app = express();
 
-const port = process.env.PORT || 3000;
-const getTheDate = date => ({
-  unix: date.valueOf(),
-  utc: date.toGMTString()
-});
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
@@ -23,21 +19,12 @@ app.get("/", (_, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/api/:date?", (req, res) => {
-  let date = req.params.date;
-  res.contentType = "application/json";
+app.get("/api/whoami", (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.acceptsLanguages().toString();
+  const software = req.get("User-Agent");
 
-  if (date === undefined)
-    return res.json(getTheDate(new Date()));
-
-
-  date = date.indexOf(",") > -1 ? decodeURIComponent(date) : date.length > 10 ? parseInt(date) : date;
-
-  const gmtDate = new Date(date);
-
-  if (gmtDate == "Invalid Date") return res.status(400).json({ error: "Invalid Date" });
-
-  res.json(getTheDate(gmtDate));
+  res.json({ ipaddress, language, software });
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
